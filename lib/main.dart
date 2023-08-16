bb584981c16f3a2830869b8d52caa3c9161bf41d
Sara <ysara4682@gmail.com>
2023-08-16 04:44:29 +0300

    request adding3

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return   ScreenUtilInit(
+      ////
       designSize: const Size(360, 690),
       minTextAdapt: true,
       splitScreenMode: true,
